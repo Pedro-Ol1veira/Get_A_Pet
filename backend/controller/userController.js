0ecdc48e3ee9e8b30f1c16550847495aa37f6aea
Pedro-Ol1veira <113263052+Pedro-Ol1veira@users.noreply.github.com>
2024-05-21 22:14:02 -0300

    check user helper

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,7 +1,9 @@
 const createUserToken = require('../helpers/create-user-token');
+const getToken = require('../helpers/get-token');
 const User = require('../models/User');
 
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 module.exports = class userController {
     static async register(req, res) {
@@ -53,13 +55,58 @@ module.exports = class userController {
             const newUser = await user.save();
             
             await createUserToken(newUser, req, res);
+
         } catch (error) {
             res.status(500).json(
                 {
                     message: error,
-                    newUser,
                 }
             );
+        };
+    };
+
+    static async login(req, res) {
+        const {email, password} = req.body;
+
+        if(!email) {
+            res.status(422).json({message: 'O e-mail é obrigatorio'});
+            return;
+        } else if (!password) {
+            res.status(422).json({message: 'A senha é obrigatoria'});
+            return;
+        };
+
+        const user = await User.findOne({email: email});
+
+        if(!user){
+            res.status(422).json({message: 'Não há usuario cadastrado com esse e-mail'});
+            return;
+        };
+
+        //check if password match
+        const checkPassword = await bcrypt.compare(password, user.password);
+
+        if(!checkPassword) {
+            res.status(422).json({message: 'Senha invalida'});
+            return;
+        };
+        
+        await createUserToken(user, req, res);
+    };
+
+    static async checkUser(req, res) {
+        let currentUser ;
+
+        if(req.headers.authorization) {
+            const token = getToken(req);
+            const decoded = jwt.verify(token, 'nossoSecret')
+
+            currentUser = await User.findById(decoded.id);
+            currentUser.password = undefined;
+        } else {
+            currentUser = null;
         }
+
+        res.status(200).send(currentUser);
     };
 };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const userController = require('../controller/userController');
 
 router.post('/register', userController.register);
+router.post('/login', userController.login);
+router.get('/checkuser', userController.checkUser);
 
 
 module.exports = router;
